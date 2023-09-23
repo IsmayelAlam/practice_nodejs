fc436f1f9be942672f9f27fca5b73d27b75449eb
Ismayel Alam <ismayelalam0@gmail.com>
2023-09-23 15:31:29 +0600

    test read and write file asynchronously

--- practice_001/index.js
+++ practice_001/index.js
@@ -1,10 +1,16 @@
 const fs = require("fs");
+const http = require("http");
 
-const testRead = fs.readFileSync("./testInput.txt", "utf-8");
-
-console.log(testRead);
-
-fs.writeFileSync(
-  "./testOutput.txt",
-  `this test is for writing on files.\n the input file includes: (${testRead})`
-);
+// const testRead = fs.readFileSync("./testInput.txt", "utf-8");
+// fs.writeFileSync(
+//   "./testOutput.txt",
+//   `this test is for writing on files.\n the input file includes: (${testRead})`
+// );
+// fs.readFile("./testInput.txt", "utf-8", (err, data) =>
+//   fs.writeFile(
+//     "./testOutput.txt",
+//     `asynchronous reading data.\nthe input file includes: (${data})`,
+//     "utf-8",
+//     (err) => console.log("written")
+//   )
+// );
