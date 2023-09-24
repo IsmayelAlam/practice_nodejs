0d6d6ff26c38920d08c9c29138d0ae87f6598b3d
Ismayel Alam <ismayelalam0@gmail.com>
2023-09-24 12:57:44 +0600

    add html templates

--- practice_001/index.js
+++ practice_001/index.js
@@ -16,6 +16,21 @@ fs.readFile("./testInput.txt", "utf-8", (err, data) =>
   )
 );
 */
+const data = fs.readFileSync(`${__dirname}/test/data.json`, "utf-8");
+const overviewTemplate = fs.readFileSync(
+  `${__dirname}/test/overviewTemplate.html`,
+  "utf-8"
+);
+const productTemplate = fs.readFileSync(
+  `${__dirname}/test/productTemplate.html`,
+  "utf-8"
+);
+const cardTemplate = fs.readFileSync(
+  `${__dirname}/test/cardTemplate.html`,
+  "utf-8"
+);
+const dataObj = JSON.parse(data);
+
 const server = http.createServer((req, res) => {
   const pathName = req.url;
 
@@ -23,16 +38,20 @@ const server = http.createServer((req, res) => {
     case "/":
       res.writeHead(200, {
         "Content-type": "text/html",
-        "test-Herder": "custom header",
       });
-      res.end("<h1>Hello world!</h1>");
+      res.end(overviewTemplate);
       return;
-    case "/overview":
-      return res.end("<h1>overview</h1>");
-    case "/test":
+
+    case "/product":
       return res.end("<h1>test</h1>");
-    case "/anotherTest":
-      return res.end("<h1>another test</h1>");
+
+    case "/API":
+      res.writeHead(200, {
+        "Content-type": "application/json",
+      });
+      res.end(data);
+      return;
+
     default:
       return res.end("<h1>404|Page not found</h1>");
   }
