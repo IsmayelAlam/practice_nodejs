b4100bead9b8ffc4ec4b565dd344dc3854a9976d
Ismayel Alam <ismayelalam0@gmail.com>
2023-09-26 18:26:23 +0600

    add post request for the custom api

--- natour/app.js
+++ natour/app.js
@@ -3,8 +3,7 @@ const fs = require('fs');
 
 const app = express();
 
-// app.get('/', (req, res) => res.send('get from the server'));
-// app.post('/', (req, res) => res.send('post data to the server'));
+app.use(express.json());
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
@@ -14,6 +13,25 @@ app.get('/api/v1/tours', (req, res) => {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: { tours } });
+});
+app.post('/api/v1/tours', (req, res) => {
+  const newID = tours.length;
+  const newTour = Object.assign({ id: newID }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 const port = 3000;
