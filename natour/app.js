2e9e9b70aef2c9627cdf16f96ee410a4f6b89346
Ismayel Alam <ismayelalam0@gmail.com>
2023-09-27 12:57:03 +0600

    refactor code

--- natour/app.js
+++ natour/app.js
@@ -9,22 +9,20 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+function getAllTours(req, res) {
   res
     .status(200)
     .json({ status: 'success', results: tours.length, data: { tours } });
-});
-
-app.get('/api/v1/tours/:id', (req, res) => {
+}
+function getTour(req, res) {
   const tour = tours.find((el) => el.id === +req.params.id);
 
   if (+req.params.id > tours.length - 1)
     res.status(404).json({ status: 'fail', message: 'invalid id' });
 
   res.status(200).json({ status: 'success', data: { tour } });
-});
-
-app.post('/api/v1/tours', (req, res) => {
+}
+function addNewTour(req, res) {
   const newID = tours.length;
   const newTour = Object.assign({ id: newID }, req.body);
 
@@ -42,16 +40,21 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
-
-app.patch('/api/v1/tours/:id', (req, res) => {
+}
+function updateTour(req, res) {
   const tour = tours[+req.params.id];
   res.status(200).json({ status: 'success', data: { tour } });
-});
-
-app.delete('/api/v1/tours/:id', (req, res) => {
+}
+function deleteTour(req, res) {
   res.status(204).json({ status: 'success', data: null });
-});
+}
+
+app.route('/api/v1/tours').get(getAllTours).post(addNewTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 const port = 3000;
 app.listen(port, () => console.log('app running'));
