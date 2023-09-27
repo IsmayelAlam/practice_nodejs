cb56b69763365da867f4010e6b9fb1be60c0b59d
Ismayel Alam <ismayelalam0@gmail.com>
2023-09-27 15:26:21 +0600

    add users routes

--- natour/app.js
+++ natour/app.js
@@ -1,18 +1,29 @@
 const express = require('express');
 const fs = require('fs');
+const morgen = require('morgan');
 
 const app = express();
 
+app.use(morgen('dev'));
+
 app.use(express.json());
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toDateString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 function getAllTours(req, res) {
-  res
-    .status(200)
-    .json({ status: 'success', results: tours.length, data: { tours } });
+  res.status(200).json({
+    status: 'success',
+    time: req.requestTime,
+    results: tours.length,
+    data: { tours },
+  });
 }
 function getTour(req, res) {
   const tour = tours.find((el) => el.id === +req.params.id);
@@ -49,12 +60,25 @@ function deleteTour(req, res) {
   res.status(204).json({ status: 'success', data: null });
 }
 
+function getAllUsers(req, res) {}
+function addNewUsers(req, res) {}
+function getUsers(req, res) {}
+function updateUsers(req, res) {}
+function deleteUsers(req, res) {}
+
 app.route('/api/v1/tours').get(getAllTours).post(addNewTour);
 app
   .route('/api/v1/tours/:id')
   .get(getTour)
   .patch(updateTour)
   .delete(deleteTour);
+
+app.route('/api/v1/users').get(getAllUsers).post(addNewUsers);
+app
+  .route('/api/v1/users/:id')
+  .get(getUsers)
+  .patch(updateUsers)
+  .delete(deleteUsers);
 
 const port = 3000;
 app.listen(port, () => console.log('app running'));
