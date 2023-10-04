8f653443d1d01e5a330c18f79d7cf6ea306c8d2b
Ismayel Alam <ismayelalam0@gmail.com>
2023-10-04 19:45:45 +0600

    add mongoose and mongodb and create test schema

--- natour/server.js
+++ natour/server.js
@@ -1,4 +1,27 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const app = require("./app");
+
+dotenv.config({ path: "./.env.local" });
+
+mongoose.connect(process.env.DATABASE).then((con) => {
+  console.log("connected");
+});
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "name needed"],
+    unique: true,
+  },
+  rating: Number,
+  price: {
+    type: Number,
+    require: [true, "price needed"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
 
 const port = 3000;
 app.listen(port, () => console.log("app running"));
