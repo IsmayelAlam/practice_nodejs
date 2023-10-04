9eb7e97bb32c8ec93332e721a16e918e922376ba
Ismayel Alam <ismayelalam0@gmail.com>
2023-10-04 20:08:30 +0600

    test database with mongodb atlas

--- natour/server.js
+++ natour/server.js
@@ -4,9 +4,7 @@ const app = require("./app");
 
 dotenv.config({ path: "./.env.local" });
 
-mongoose.connect(process.env.DATABASE).then((con) => {
-  console.log("connected");
-});
+mongoose.connect(process.env.DATABASE).then((con) => {});
 
 const tourSchema = new mongoose.Schema({
   name: {
@@ -21,7 +19,18 @@ const tourSchema = new mongoose.Schema({
   },
 });
 
-const Tour = mongoose.model("Tour", tourSchema);
+const Tour = mongoose.model("Tours", tourSchema);
+
+const testTour = new Tour({
+  name: "test 01",
+  rating: 4.7,
+  price: 499,
+});
+
+testTour
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.error(err));
 
 const port = 3000;
 app.listen(port, () => console.log("app running"));
