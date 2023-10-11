07882262e8cb8a03e8388165d3057de4ab6a9f60
Ismayel Alam <ismayelalam0@gmail.com>
2023-10-11 20:09:14 +0600

    add global error handler

--- natour/app.js
+++ natour/app.js
@@ -17,10 +17,24 @@ app.use((req, res, next) => {
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRoutes);
 
-app.all("*", (req, res) => {
-  res
-    .status(404)
-    .json({ status: "fail", message: `can't find ${req.originalUrl}` });
+app.all("*", (req, res, next) => {
+  // res
+  //   .status(404)
+  //   .json({ status: "fail", message: `can't find ${req.originalUrl}` });
+
+  const err = new Error(`can't find ${req.originalUrl}`);
+
+  err.statuscode = 404;
+  err.status = "fail";
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+  res.status(err.statusCode).json({ status: err.status, message: err.message });
 });
 
 module.exports = app;
