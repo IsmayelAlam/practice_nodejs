fcf54001162a7ca520d7acc20dd9faa5856f5419
Ismayel Alam <ismayelalam0@gmail.com>
2023-10-14 11:36:44 +0600

    add password validation and encryption

--- natour/models/userModel.js
+++ natour/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 const validator = require("validator");
 
 const userSchema = new mongoose.Schema({
@@ -12,11 +13,11 @@ const userSchema = new mongoose.Schema({
     required: [true, "A user needs email"],
     unique: true,
     lowercase: true,
-    // validate: [validator.isEmail, "please provide a valid email"],
-    match: [
-      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-      "please provide a valid email",
-    ],
+    validate: [validator.isEmail, "please provide a valid email"],
+    // match: [
+    //   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+    //   "please provide a valid email",
+    // ],
   },
   photo: String,
   password: {
@@ -27,7 +28,21 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, "A user needs password confirmation"],
+    message: "passwords are not same",
+    validate: {
+      validator: function (el) {
+        return this.password === el;
+      },
+    },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
