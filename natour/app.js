a40db88e6db5e50f20559db41a778f71c771fb86
Ismayel Alam <ismayelalam0@gmail.com>
2023-10-16 13:35:00 +0600

    sanitize input data

--- natour/app.js
+++ natour/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgen = require("morgan");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
 const helmet = require("helmet");
 const rateLimit = require("express-rate-limit");
 const tourRoutes = require("./routes/tourRoutes");
@@ -25,6 +27,9 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toDateString();
   next();
 });
+
+app.use(mongoSanitize());
+app.use(xss());
 
 app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tours", tourRoutes);
